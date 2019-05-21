fef6a8334ea29cad475aa1897c20331403643194
Daniel <dlopez@soulecommerce.com>
2019-05-21 23:38:09 +0200

    Updates

---
+++ precache-manifest.58b66fc168b9fcf03e33d467191e0eda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e13bc9e6ac19431ec7d981076ffbe7",
+    "url": "/DareJump/index.html"
+  },
+  {
+    "revision": "2a9fd0ed17b7b2baf094",
+    "url": "/DareJump/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/DareJump/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2a9fd0ed17b7b2baf094",
+    "url": "/DareJump/static/js/main.3e41171a.chunk.js"
+  },
+  {
+    "revision": "ca3cc4515f1524ec3be9",
+    "url": "/DareJump/static/js/runtime~main.81ce4a76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DareJump/static/media/logo.5d5d9eef.svg"
+  }
+]);
